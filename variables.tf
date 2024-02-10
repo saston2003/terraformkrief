1cd3e6612aaa952081f802581ac040e786196373
saston2003@outlook.com <saston2003@outlook.com>
2024-02-10 20:33:15 +0000

    my commit

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "resoure_group_name" {
+  description = "Name of the resource group"
+}
+
+variable "location" {
+  description = "Location of the resource"
+  default     = "West Europe"
+}
+
+variable "application_name" {
+  description = "Name of the application"
+}
+variable "ip_name" {
+  description = "Name of the ip address"
+}
